b00ecb5237acfc0584beb42c365c54db71e41df1
Alan Coon <alancoon@usc.edu>
2017-03-14 23:55:58 -0700

    Long way, doesn't seem to quite work.

--- assign2/pinger.py
+++ assign2/pinger.py
@@ -8,6 +8,7 @@ alancoon@usc.edu
 
 import sys
 import socket
+from struct import *
 
 def pinger ():
 
@@ -17,56 +18,48 @@ def pinger ():
 	flag_used['l'] = False
 	flag_used['c'] = False
 
+	logfile 	= None
+	payload 	= None
+	count  		= None
+	destination = None
+
 	for index, word in enumerate(sys.argv):
 
 		if (word[0] == '-'):
 			flag = word[1].lower()
 			try:
-				next_word = sys.argv[index + 1]
-				if next_word[0] == '-':
+				next_arg = sys.argv[index + 1]
+				if next_arg[0] == '-':
 					raise
 			except:
 				print_instructions()
 				sys.exit()
 
-
 			if (flag == 'd' or flag == '-dst'):
-				destination = next_word
+				destination = next_arg
 				flag_used['d'] = True
 
 			elif (flag == 'p' or flag == '-payload'):
-				# Combine the argv into one string then split by double-quotes.
-				print sys.argv
-				print ' '.join(sys.argv)
-				argv_split = (' '.join(sys.argv)).split('\"')
-				if (len(argv_split) != 3):
-					# If the size of the array isn't 3, which means we have two double-quotes,
-					# then we know something is wrong because the user is putting more
-					# double-quotes than he or she should.
-					payload = argv_split[1]
-				else:
-					print_instructions()
-					sys.exit()
+				payload = next_arg
+				flag_used['p'] = True
 
 			elif (flag == 'l' or flag == '-logfile'):
-				logfile = next_word
+				logfile = next_arg
 				flag_used['l'] = True
 
 			elif (flag == 'c' or flag == '-count'):
-				count = next_word
+				count = next_arg
 				flag_used['c'] = True
 
 			else:
 				print_instructions()
 				sys.exit()
 
-	check_validity(flag_used)
 
-	if (flag_used['l']):
-		print 'logfile: ' + logfile
-	print 'payload: ' + payload
-	print 'count: ' + count
-	print 'destination: ' + destination
+	check_validity(flag_used, destination, payload, logfile, count)
+
+
+
 
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
@@ -75,6 +68,7 @@ def pinger ():
 		print 'Socket could not be created. Error code: ' + str(msg[0]) + ' Message: ' + msg[1]
 		sys.exit()
 
+	print 'Pinging ' + destination + ' with ' + len(payload) + ' bytes of data \"' + payload + '\"'
 
 	source = socket.gethostbyname(socket.gethostname())
 
@@ -83,7 +77,7 @@ def pinger ():
 	ip_ver = 4
 	ip_tos = 0
 	ip_tot_len = 0  # kernel will fill the correct total length
-	ip_id = 54321   #Id of this packet
+	ip_id = 0   #Id of this packet
 	ip_frag_off = 0
 	ip_ttl = 255
 	ip_proto = socket.IPPROTO_TCP
@@ -92,6 +86,32 @@ def pinger ():
 	ip_daddr = socket.inet_aton(destination)
 
 	ip_ihl_ver = (ip_ver << 4) + ip_ihl
+
+	# the ! in the pack format string means network order
+	ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+
+
+	# tcp header fields
+	tcp_source = 1234   # source port
+	tcp_dest = 80   # destination port
+	tcp_seq = 454
+	tcp_ack_seq = 0
+	tcp_doff = 5    #4 bit field, size of tcp header, 5 * 4 = 20 bytes
+	#tcp flags
+	tcp_fin = 0
+	tcp_syn = 1
+	tcp_rst = 0
+	tcp_psh = 0
+	tcp_ack = 0
+	tcp_urg = 0
+	tcp_window = socket.htons (5840)    #   maximum allowed window size
+	tcp_check = 0
+	tcp_urg_ptr = 0
+	 
+	tcp_offset_res = (tcp_doff << 4) + 0
+	tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
+
+
 	tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
 
 	# Header fields
@@ -99,10 +119,10 @@ def pinger ():
 	destination_address = socket.inet_aton(destination)
 	placeholder = 0
 	protocol = socket.IPPROTO_TCP
-	tcp_length = len(tcp_header) + len(user_data)
+	tcp_length = len(tcp_header) + len(payload)
 	 
 	psh = pack('!4s4sBBH' , source_address , destination_address , placeholder , protocol , tcp_length);
-	psh = psh + tcp_header + user_data;
+	psh = psh + tcp_header + payload;
 	 
 	tcp_check = 1 # checksum(psh)
 	 
@@ -110,16 +130,30 @@ def pinger ():
 	tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
 	 
 	# final full packet - syn packets dont have any data
-	packet = ip_header + tcp_header + user_data
+	packet = ip_header + tcp_header + payload
 	 
 	#Send the packet finally - the port specified has no effect
 	s.sendto(packet, (destination , 0 ))    # put this in a loop if you want to flood the target
 
-def check_validity (flags):
-	if not flags['c'] or not flags['p'] or not flags['d']:
+def check_validity (flags, d, p, l, c):
+	print flags
+	if flags['c'] and flags['p'] and flags['d']:
+		print 'Validity check passed'
+	else:
+		print 'Validity check failed'
+		if (flags['l']):
+			print 'logfile: ' + l
+		print 'payload: ' + p
+		print 'count: ' + c
+		print 'destination: ' + d
 		print_instructions()
 		sys.exit()
 
+def checksum(data):
+    x = sum(x << 8 if i % 2 else x for i, x in enumerate(data)) & 0xFFFFFFFF
+    x = (x >> 16) + (x & 0xFFFF)
+    x = (x >> 16) + (x & 0xFFFF)
+    return struct.pack('<H', ~x & 0xFFFF)
 
 def print_instructions ():
 	print 'pinger [-l file] -p \"data\" -c N -d IP'
